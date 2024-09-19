b0354075c4466142745ad39df52a186cb0d48c0e
hyunseo <wndlthsk@naver.com>
2024-09-19 17:50:27 +0900

    initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${app.version:1.0.0}")
+    String version;
+
+    @GetMapping("/")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("Hello world!"+version);
+    }
+}
